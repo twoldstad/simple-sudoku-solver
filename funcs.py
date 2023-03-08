3a63d857b6303fe0372730f3d46040114306969c
twoldstad <45495467+twoldstad@users.noreply.github.com>
2023-03-08 13:00:58 -0800

    Fixed some bugs, added tests.

--- funcs.py
+++ funcs.py
@@ -3,24 +3,24 @@ from math import sqrt
 class Puzzle:
     def __init__(self, grid: list) -> None:
         self.grid = grid
-        self.grid_size = len(grid)  # must be a square number
-        self.box_size = sqrt(self.grid_size)
+        self.grid_size = int(sqrt(len(grid)))  # must be a square number
+        self.box_size = int(sqrt(self.grid_size))
 
         if not int(sqrt(self.grid_size))**2 == self.grid_size:
             raise ValueError('Grid size is not a square number.')
 
     def row_checker(self, index: int) -> bool:
         """ finds the current row, counts the unique values, then compares that number with the length of the original row """
-        return len(set(int(x) for x in self.grid[index//self.grid_size * self.grid_size:(index//self.grid_size + 1) * self.grid_size] if x)) == len([x for x in self.grid[index//self.grid_size * self.grid_size:(index//self.grid_size + 1) * self.grid_size] if x])
+        return len(set(int(x) for x in self.grid[(index//self.grid_size * self.grid_size):(index//self.grid_size + 1) * self.grid_size] if x)) == len([x for x in self.grid[index//self.grid_size * self.grid_size:(index//self.grid_size + 1) * self.grid_size] if x])
 
     def column_checker(self, index: int) -> bool:
         """ finds the current column, counts the unique values, then compares that number with the length of the original column """
-        return len(set(int(x) for x in self.grid[index % self.grid_size::self.grid_size] if x)) == len([x for x in self.grid[index % self.grid_size::self.grid_size] if x])
+        return len(set(int(x) for x in self.grid[(index % self.grid_size)::self.grid_size] if x)) == len([x for x in self.grid[index % self.grid_size::self.grid_size] if x])
 
     def box_checker(self, index: int) -> bool:
         """ finds the current box, counts the unique values, then compares that number with the length of the original box """
         gs, bs = self.grid_size, self.box_size
-        start = index//gs//bs * (bs * gs) + (index % gs)//bs * bs   # start = box level * box size + box column * box size 
+        start = index//gs//bs * (bs * gs) + (index % gs)//bs * bs   # start = box level * box size + box column * box size
         box = []
         [[box.append(x) for x in self.grid[start+i*gs:start+i*gs+bs]] for i in range(0,3)]
         return len(set(int(x) for x in box if x)) == len([x for x in box if x])
